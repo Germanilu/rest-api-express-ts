0cb272e92ae506d7521b4006dd675da29847c10b
Luciano Germani <lucianogermani93@gmail.com>
2025-05-21 13:27:48 +0200

    Feat: addd env variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,10 @@
 import {Sequelize} from 'sequelize';
+import dotenv from 'dotenv';
 
-// Conexion al db, se le añade al final de la url ?ssl=true para que no de error al conectar
-const db = new Sequelize('postgresql://rest_api_node_typescript_suze_user:FdMTU9GpKb8Uimu72uSjtJUV46IrhMA9@dpg-d0mr6mmuk2gs73ftolc0-a.frankfurt-postgres.render.com/rest_api_node_typescript_suze?ssl=true')
+//Mando a llamar las variables de entorno
+dotenv.config()
+
+// Conexion al db, se le añade al final de la url ?ssl=true para que no de error al conectar(ver .env)
+const db = new Sequelize(process.env.DATABASE_URL!)
 
 export default db;
