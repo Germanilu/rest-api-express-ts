8c8f2d2784f2cb74854ead42580c1695f0097362
Luciano Germani <lucianogermani93@gmail.com>
2025-05-21 15:05:28 +0200

    Feat: creating model/ db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,12 @@
-import {Sequelize} from 'sequelize';
+import {Sequelize} from 'sequelize-typescript';
 import dotenv from 'dotenv';
 
 //Mando a llamar las variables de entorno
 dotenv.config()
 
 // Conexion al db, se le añade al final de la url ?ssl=true para que no de error al conectar(ver .env)
-const db = new Sequelize(process.env.DATABASE_URL!)
+const db = new Sequelize(process.env.DATABASE_URL!, {
+    models: [__dirname + '/../models/**/*.ts']
+})
 
 export default db;
