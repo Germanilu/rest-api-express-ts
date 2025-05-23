02de685e1539db4ac31def95a3ac28a6c71d7afe
Luciano Germani <lucianogermani93@gmail.com>
2025-05-23 15:57:29 +0200

    TESTING: Añadida todas las validaciones crear producto

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -15,6 +15,36 @@ describe('POST /api/products', () => {
     })
 
 
+    it('Should validate that the price is greater than 0', async () => {
+        //Conecto al sv y hago un post a la url /api/product enviandole el body VACIO
+        const response = await request(server).post('/api/products').send({
+            name: "Monitor Muy Curvo",
+            price: 0
+        })
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(1)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+
+    it('Should validate that the price is a Number and greater than 0', async () => {
+        //Conecto al sv y hago un post a la url /api/product enviandole el body VACIO
+        const response = await request(server).post('/api/products').send({
+            name: "Monitor Muy Curvo",
+            price: "hola"
+        })
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(2)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(4)
+    })
+
+
     it('Should create a new Product', async () => {
         //Conecto al sv y hago un post a la url /api/product enviandole el body
         const response = await request(server).post('/api/products').send({
@@ -23,7 +53,7 @@ describe('POST /api/products', () => {
         })
         expect(response.status).toBe(201)
         expect(response.body).toHaveProperty('data')
-        
+
         expect(response.status).not.toBe(400)
         expect(response.status).not.toBe(404)
         expect(response.body).not.toHaveProperty('errors')
