60ea6376bed130a006041c90f6a443dda654d47c
Luciano Germani <lucianogermani93@gmail.com>
2025-05-23 15:50:44 +0200

    JEST: Testing Validation Error

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -2,6 +2,19 @@ import request from 'supertest';
 import server from '../../server';
 
 describe('POST /api/products', () => {
+
+    it('Should Display Validation Error', async () => {
+        //Conecto al sv y hago un post a la url /api/product enviandole el body VACIO
+        const response = await request(server).post('/api/products').send({})
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty('errors')
+        expect(response.body.errors).toHaveLength(4)
+
+        expect(response.status).not.toBe(404)
+        expect(response.body.errors).not.toHaveLength(2)
+    })
+
+
     it('Should create a new Product', async () => {
         //Conecto al sv y hago un post a la url /api/product enviandole el body
         const response = await request(server).post('/api/products').send({
@@ -13,6 +26,6 @@ describe('POST /api/products', () => {
         
         expect(response.status).not.toBe(400)
         expect(response.status).not.toBe(404)
-        expect(response.body).not.toHaveProperty('error')
+        expect(response.body).not.toHaveProperty('errors')
     })
 })
